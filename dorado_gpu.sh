4260051cdb8e23b05b630496fd3fede9176f7688
mallakshat <70936499+mallakshat@users.noreply.github.com>
2026-05-16 12:50:18 -0700

    Updated slurm script

--- dorado_gpu.sh
+++ dorado_gpu.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=dorado_gpu_job        # Job name
-#SBATCH --error=dorado_gpu_error.txt     # Standard error file
+#SBATCH --error=dorado_gpu_error.txt     # error file
 #SBATCH --ntasks=1                       # Number of tasks (single task)
-#SBATCH --cpus-per-task=16               # Number of CPU cores per task
-#SBATCH --nodes=1       	         # Request 1 nodes
-#SBATCH --gres=gpu:2    	         # 2 GPU per node
+#SBATCH --nodes=1       	             # Request 1 nodes
+#SBATCH --gres=gpu:2    	             # 2 GPU per node
 #SBATCH --partition=gpu-8                # Use the 'gpu-8' partition
-#SBATCH --time=120:00:00                 # Time limit (5 days)
+#SBATCH --time=24:00:00                  # Time limit (1 day)
 
 module load cuda
 export CUDA_VISIBLE_DEVICES=$(echo $SLURM_JOB_GPUS | tr ',' ' ')
